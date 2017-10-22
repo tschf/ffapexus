405e9e444bdf7c227db3b66114fa2882cb04823b
Trent Schafer <trent.schafer@gmail.com>
2017-10-22 15:21:29 -0400

    Add some comments about isFirefox

--- ffapexus.js
+++ ffapexus.js
@@ -7,12 +7,14 @@
 // InstallTrigger is interface specifically for Firefox. By
 // checking if this is available, we can ascertain if we are
 // in Firefox. Idea from: https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+// Since this is installed via a user script, the Firefox
+// test shouldn't be strictly necessary, however it can't
+// harm having an additional check in there.
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
 if (isFirefox) {
     // Store the original version of function so that we can re-call in our custom implementation
     var origOpenInNewWindow = apex.navigation.openInNewWindow;
-    
     
     apex.navigation.openInNewWindow = function tschfOpenInNewWindow(pURL, pWindowName, pOptions) { 
         var parent = origOpenInNewWindow(pURL, pWindowName, pOptions); 
